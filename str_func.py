8ee2e92dc417ca7625ada94642c543319f71b710
Veronics S <korolova27@rambler.ru>
2023-09-03 14:31:07 +0300

    add_str_func

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
-#функция перевода слова в заглавные буквы
+
 def capital_letters(lines):
     return lines.upper()
 
+def tittle_word(phrase):
+    """"функция все слова с заглавной буквы"""
+    return phrase.title()
