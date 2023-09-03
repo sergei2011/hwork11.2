f7e6a750054e0edcab09ee41bf32dc43a7cdb43d
sergei2011 <143061675+sergei2011@users.noreply.github.com>
2023-09-03 14:32:45 +0300

    add_str_func (#3)
    Co-authored-by: Veronics S <korolova27@rambler.ru>

--- str_func.py
+++ str_func.py
@@ -3,3 +3,6 @@ def capital_letters(lines):
     """функция перевода слова в заглавные буквы""""
     return lines.upper()
 
+def tittle_word(phrase):
+    """"функция все слова с заглавной буквы"""
+    return phrase.title()
